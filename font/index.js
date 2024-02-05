5f3eedfaf5ce7963e808529f48ad44e3af3c3c8e
bbdataviz <74002681+bbdataviz@users.noreply.github.com>
2024-02-05 13:56:25 +0100

    Update index.js

--- font/index.js
+++ font/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const cors = require('cors');
 
-app.use()
+app.use(cors({ origin: 'https://github.com/bbdataviz/HG-website/tree/main/font'}))
 
 app.get('/', (req, res) => {
     res.status(200).json({ title: 'hello world'});
